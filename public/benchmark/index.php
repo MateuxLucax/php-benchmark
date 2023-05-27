baddaf35b80c273f5affb16463c38dde00af880c
Mateus Lucas <mateuxlucax@gmail.com>
2023-05-27 19:54:38 -0300

    feat: add multiplier on benchmark

--- public/benchmark/index.php
+++ public/benchmark/index.php
@@ -2,15 +2,18 @@
 // Benchmark.php
 $start = microtime(true);
 
+// Get multiplier from GET parameter, default to 1 if not set
+$multiplier = isset($_GET['multiplier']) ? $_GET['multiplier'] : 1;
+
 // Test math operations
-for ($i = 0; $i < 1000000; $i++) {
+for ($i = 0; $i < 1000000 * $multiplier; $i++) {
     $a = $i * $i;
     $b = sqrt($a);
 }
 
 // Test string operations
 $string = '';
-for ($i = 0; $i < 100000; $i++) {
+for ($i = 0; $i < 100000 * $multiplier; $i++) {
     $string .= 'a';
     $string = md5($string);
 }
