50634227ea4ce60e331ed522d3a07fb1e731f510
Mateus Lucas <mateuxlucax@gmail.com>
2023-05-27 21:37:05 -0300

    refac: improve stats screen

--- public/stats/index.php
+++ public/stats/index.php
@@ -1,22 +1,75 @@
 <?php
-$memory = shell_exec('free -m');
-echo "Memory usage:\n" . $memory . "\n";
+    $meminfo = shell_exec('free -m');
+    $uptime = shell_exec('uptime');
+    $memlines = explode("\n", $meminfo);
+    $temp = shell_exec('cat /sys/class/thermal/thermal_zone0/temp');
+    $temp = $temp / 1000;
 
-$load = shell_exec('uptime');
-echo "CPU load:\n" . $load . "\n";
+    $memory = array();
+    foreach ($memlines as $line) {
+        $parts = preg_split('/\s+/', $line);
+        if(count($parts) >= 7) {
+            $memory[$parts[0]] = array(
+                'total' => $parts[1],
+                'used' => $parts[2],
+                'free' => $parts[3],
+                'shared' => $parts[4],
+                'buff/cache' => $parts[5],
+                'available' => $parts[6]
+            );
+        }
+    }
 
-$disk = shell_exec('df -h');
-echo "Disk usage:\n" . $disk . "\n";
+    $uptime = preg_replace('/\s+/', ' ', $uptime);
+?>
 
-$network = shell_exec('netstat -i');
-echo "Network info:\n" . $network . "\n";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Information</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h2>Server Information</h2>
 
-$system = shell_exec('uname -a');
-echo "System info:\n" . $system . "\n";
+    <h3>Memory Usage (in MB)</h3>
+    <table>
+        <tr>
+            <th></th>
+            <th>Total</th>
+            <th>Used</th>
+            <th>Free</th>
+            <th>Shared</th>
+            <th>Buff/Cache</th>
+            <th>Available</th>
+        </tr>
+        <?php foreach($memory as $key => $value): ?>
+            <tr>
+                <td><?= $key; ?></td>
+                <td><?= $value['total']; ?></td>
+                <td><?= $value['used']; ?></td>
+                <td><?= $value['free']; ?></td>
+                <td><?= $value['shared']; ?></td>
+                <td><?= $value['buff/cache']; ?></td>
+                <td><?= $value['available']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
-$processes = shell_exec('ps aux');
-echo "Running processes:\n" . $processes . "\n";
+    <h3>CPU Load and Uptime</h3>
+    <p><?= $uptime; ?></p>
 
-$temp = shell_exec('cat /sys/class/thermal/thermal_zone0/temp');
-$temp = $temp / 1000;
-echo "CPU temperature: " . $temp . "°C";
+    <h3>CPU Temperature</h3>
+    <p><?= $temp . "°C" ?></p>
+</body>
+</html>
